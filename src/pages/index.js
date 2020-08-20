f66919c145a3c3cb2b4b976180981c37743635a1
Puneet Pareenja <pareenjapuneet@gmail.com>
2020-08-19 17:11:32 -0700

    Added Sass. Removed unused imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import Footer from "../components/footer"
-import Header from "../components/header"
 import Layout from "../components/layout"
 
 const IndexPage = () => {
